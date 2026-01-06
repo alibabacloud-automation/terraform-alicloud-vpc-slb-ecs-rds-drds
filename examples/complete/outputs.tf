cc9166de08895a76df170df23eb1b6d18fcef28b
shanye <shanye.xzq@alibaba-inc.com>
2026-01-06 11:01:12 +0800

    feat!: remove unused variables; improve the examples

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,15 +1,19 @@
 output "this_ecs_name" {
-  value = module.complete.this_ecs_name
+  description = "The name of the ECS instance."
+  value       = module.complete.this_ecs_name
 }
 
 output "this_ecs_id" {
-  value = module.complete.this_ecs_id
+  description = "The ID of the ECS instance."
+  value       = module.complete.this_ecs_id
 }
 
 output "this_rds_id" {
-  value = module.complete.this_rds_id
+  description = "The ID of the RDS instance."
+  value       = module.complete.this_rds_id
 }
 
 output "this_slb_id" {
-  value = module.complete.this_slb_id
+  description = "The ID of the SLB instance."
+  value       = module.complete.this_slb_id
 }
